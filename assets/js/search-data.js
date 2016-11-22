26286e08e017e8574cd05eccf6f7e85ff9c0599d
Steve Barnett <steve@naga.co.za>
2016-11-22 06:38:44 +0200

    Move search index and store to own file

---
+++ assets/js/search-data.js
@@ -0,0 +1,44 @@
+---
+layout: null
+---
+
+// set up elasticlunr
+var index = elasticlunr(function () {
+    this.addField('title');
+    this.addField('content');
+});
+
+// add the data to the index
+{% assign count = 0 %}
+{% for page in site.pages %}
+  {% unless
+  page.url contains ".css" or
+  page.url contains ".js" or
+  page.url contains "file-list" or
+  page.url contains "search.html"
+  %}
+  index.addDoc({
+    id: {{count}},
+    title: {{page.title | jsonify}},
+    content: {{page.content | strip_html | jsonify}},
+  });
+  {% assign count = count | plus: 1 %}
+  {% endunless %}
+{% endfor %}
+
+// add data to a store, since elasticlunr only returns (0-based indexed) `ref`
+var store = [
+  {% for page in site.pages %}
+    {% unless
+    page.url contains ".css" or
+    page.url contains ".js" or
+    page.url contains "file-list" or
+    page.url contains "search.html"
+    %}{
+    'title': {{page.title | jsonify}},
+    'content': {{page.content | strip_html | jsonify}},
+    'url': {{ page.url | jsonify}}
+   }{% unless forloop.last %},{% endunless %}
+   {% endunless %}
+ {% endfor %}
+];
--- assets/js/search.js
+++ assets/js/search.js
@@ -2,47 +2,6 @@
 layout: null
 ---
 
-// set up elasticlunr
-var index = elasticlunr(function () {
-    this.addField('title');
-    this.addField('content');
-});
-
-// add the data to the index
-{% assign count = 0 %}
-{% for page in site.pages %}
-  {% unless
-  page.url contains ".css" or
-  page.url contains ".js" or
-  page.url contains "file-list" or
-  page.url contains "search.html"
-  %}
-  index.addDoc({
-    id: {{count}},
-    title: {{page.title | jsonify}},
-    content: {{page.content | strip_html | jsonify}},
-  });
-  {% assign count = count | plus: 1 %}
-  {% endunless %}
-{% endfor %}
-
-// add data to a store, since elasticlunr only returns (0-based indexed) `ref`
-var store = [
-  {% for page in site.pages %}
-    {% unless
-    page.url contains ".css" or
-    page.url contains ".js" or
-    page.url contains "file-list" or
-    page.url contains "search.html"
-    %}{
-    'title': {{page.title | jsonify}},
-    'content': {{page.content | strip_html | jsonify}},
-    'url': {{ page.url | jsonify}}
-   }{% unless forloop.last %},{% endunless %}
-   {% endunless %}
- {% endfor %}
-];
-
 var searchResults = document.getElementById('search-results'),
     searchBox = document.getElementById('search-box');
 
