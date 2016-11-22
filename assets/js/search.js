585158b23553a76d5af8fc255e1598989704f395
Steve Barnett <steve@naga.co.za>
2016-11-22 06:22:10 +0200

    Exclude assets from search results and store

--- assets/js/search.js
+++ assets/js/search.js
@@ -11,20 +11,36 @@ var index = elasticlunr(function () {
 // add the data to the index
 {% assign count = 0 %}
 {% for page in site.pages %}
-index.addDoc({
-  id: {{count}},
-  title: {{page.title | jsonify}},
-  content: {{page.content | strip_html | jsonify}},
-});
-{% assign count = count | plus: 1 %}
+  {% unless
+  page.url contains ".css" or
+  page.url contains ".js" or
+  page.url contains "file-list" or
+  page.url contains "search.html"
+  %}
+  index.addDoc({
+    id: {{count}},
+    title: {{page.title | jsonify}},
+    content: {{page.content | strip_html | jsonify}},
+  });
+  {% assign count = count | plus: 1 %}
+  {% endunless %}
 {% endfor %}
 
 // add data to a store, since elasticlunr only returns (0-based indexed) `ref`
-var store = [{% for page in site.pages %}{
-  'title': {{page.title | jsonify}},
-  'content': {{page.content | strip_html | jsonify}},
-  'url': {{ page.url | jsonify}}
- }{% unless forloop.last %},{% endunless %}{% endfor %}
+var store = [
+  {% for page in site.pages %}
+    {% unless
+    page.url contains ".css" or
+    page.url contains ".js" or
+    page.url contains "file-list" or
+    page.url contains "search.html"
+    %}{
+    'title': {{page.title | jsonify}},
+    'content': {{page.content | strip_html | jsonify}},
+    'url': {{ page.url | jsonify}}
+   }{% unless forloop.last %},{% endunless %}
+   {% endunless %}
+ {% endfor %}
 ];
 
 // get query search term from GET query string
