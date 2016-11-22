04c49779d231d43a0edb2ac207420f134fab9fa4
Steve Barnett <steve@naga.co.za>
2016-11-22 06:30:54 +0200

    Move search term grabber to own module

---
+++ assets/js/get-query-variable.js
@@ -0,0 +1,13 @@
+// get query search term from GET query string
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+    }
+  }
+}
