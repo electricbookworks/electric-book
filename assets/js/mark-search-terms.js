39c629308c4ec23a9d8f353b51ca9a2629dbc10c
Steve Barnett <steve@naga.co.za>
2016-11-24 16:24:39 +0200

    Wrap searchTerm marking

--- assets/js/mark-search-terms.js
+++ assets/js/mark-search-terms.js
@@ -1,3 +1,5 @@
 // set up for mark.js to do its thing
 var markInstance = new Mark(document.querySelector("#wrapper"));
-markInstance.unmark().mark(searchTerm);
+if (searchTerm) {
+  markInstance.unmark().mark(searchTerm);
+}
