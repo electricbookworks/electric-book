eb5f35ba2f832b3097d6dc1429a66c6163650093
Arthur Attwell <arthur@arthurattwell.com>
2017-02-11 13:48:53 +0200

    Adds a close button to nav Thanks to @stevebarnett for this

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -20,9 +20,14 @@
       // hide the menu until we click the link
       menu.classList.add("visuallyhidden");
 
+      // add a close button
+      var closeButton = '<button data-toggle data-nav-close><span class="visuallyhidden">Close menu</span></button>';
+      menu.insertAdjacentHTML('afterBegin', closeButton);
+      var closeButtonInDOM = document.querySelector('[data-nav-close]');
+
       // hide the children and add the button for toggling
       var subMenus = document.querySelectorAll('#nav .has-children, #nav .has-children');
-      var showChildrenButton = '<button data-toggle-nav><span class="visuallyhidden">Toggle children</button>';
+      var showChildrenButton = '<button data-toggle data-toggle-nav><span class="visuallyhidden">Toggle children</span></button>';
       for (var i = 0; i < subMenus.length; i++) {
         subMenus[i].querySelector('ol, ul').classList.add('visuallyhidden');
         subMenus[i].querySelector('a').insertAdjacentHTML('afterend', showChildrenButton);
@@ -35,14 +40,29 @@
         document.documentElement.classList.toggle('js-nav-open');
       }, true);
 
-      // show the children when we click a .has-children
+      var hideMenu = function() {
+          menu.classList.add("visuallyhidden");
+          document.documentElement.classList.remove('js-nav-open');
+      }
+
+      // listen for clicks inside the menu
       menu.addEventListener("click", function(ev) {
-        if (ev.target.hasAttribute("data-toggle-nav")) {
-          ev.preventDefault();
-          ev.target.classList.toggle('show-children');
-          ev.target.nextElementSibling.classList.toggle('visuallyhidden');
+        if (ev.srcElement.parentNode.classList.contains("active")
+            && ev.srcElement.tagName === "A") {
+            // if it's an on-page anchor, hide the menu
+            hideMenu();
+        } else if (ev.srcElement.hasAttribute("data-nav-close")) {
+            // hide the menu when we click the button
+            ev.preventDefault();
+            hideMenu();
+        } else  if (ev.srcElement.hasAttribute("data-toggle-nav")) {
+            // show the children when we click a .has-children
+            ev.preventDefault();
+            ev.srcElement.classList.toggle('show-children');
+            ev.srcElement.nextElementSibling.classList.toggle('visuallyhidden');
         }
-      }, true)
+
+        });
 
     }
 
