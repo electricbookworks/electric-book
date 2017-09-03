de1d62c4c8c86e58474b0fbda959c986c1db41a5
Arthur Attwell <arthur@arthurattwell.com>
2017-09-03 11:28:33 +0200

    Add docs to search index, add fallback to missing titles

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,11 +1,27 @@
+// get electric-book metadata
+{% include metadata %}
+
 // set up elasticlunr
 var index = elasticlunr(function () {
     this.addField('title');
     this.addField('content');
 });
 
-// add the data to the index
 {% assign count = 0 %}
+
+// add the docs to the index, if there are docs
+{% if output-docs == "true" %}
+  {% for page in site.docs %}
+    index.addDoc({
+      id: {{count}},
+      title: {% if page.title and page.title != "" %}{{ page.title | jsonify}}{% else %}{{ page.url | replace: "/"," " | jsonify}}{% endif %},
+      content: {{page.content | strip_html | jsonify}},
+    });
+    {% assign count = count | plus: 1 %}
+  {% endfor %}
+{% endif %}
+
+// add the pages to the index
 {% for page in site.pages %}
   {% unless
   page.url contains ".css" or
@@ -20,7 +36,7 @@ var index = elasticlunr(function () {
   %}
   index.addDoc({
     id: {{count}},
-    title: {{page.title | jsonify}},
+    title: {% if page.title and page.title != "" %}{{ page.title | jsonify}}{% else %}{{ page.url | replace: "/"," " | jsonify}}{% endif %},
     content: {{page.content | strip_html | jsonify}},
   });
   {% assign count = count | plus: 1 %}
@@ -29,6 +45,13 @@ var index = elasticlunr(function () {
 
 // add data to a store, since elasticlunr only returns (0-based indexed) `ref`
 var store = [
+  {% for page in site.docs %}
+    {
+      'title': {% if page.title and page.title != "" %}{{ page.title | jsonify}}{% else %}{{ page.url | replace: "/"," " | jsonify}}{% endif %},
+      'excerpt': {{ page.content | split: "<p>" | shift | first | truncatewords: 20, "&hellip;" | strip_html | jsonify}},
+      'url': {{ page.url | jsonify}}
+    },
+  {% endfor %}
   {% for page in site.pages %}
     {% unless
     page.url contains ".css" or
@@ -41,10 +64,10 @@ var store = [
     page.url contains "contents.html" or
     page.url contains "preface.html"
     %}{
-    'title': {{ page.title | jsonify}},
-    'excerpt': {{ page.content | split: "<p>" | shift | first | truncatewords: 20, "&hellip;" | strip_html | jsonify}},
-    'url': {{ page.url | jsonify}}
-   }{% unless forloop.last %},{% endunless %}
-   {% endunless %}
- {% endfor %}
+      'title': {% if page.title and page.title != "" %}{{ page.title | jsonify}}{% else %}{{ page.url | replace: "/"," " | jsonify}}{% endif %},
+      'excerpt': {{ page.content | split: "<p>" | shift | first | truncatewords: 20, "&hellip;" | strip_html | jsonify}},
+      'url': {{ page.url | jsonify}}
+    }{% unless forloop.last %},{% endunless %}
+    {% endunless %}
+  {% endfor %}
 ];
