8bd1deeb7f42607ecd77dfe4c58f77b1ee755e61
Arthur Attwell <arthur@arthurattwell.com>
2018-09-20 14:06:12 +0200

    JS work in progress

--- assets/js/annotation.js
+++ assets/js/annotation.js
@@ -1,32 +1,93 @@
-// highlighting must check toggle and highlight checkboxes
+// Let JS Lint allow window global variable
+/*global window */
 
+// This script shows and hide the annotation sidebar
+// and the annotations in the page text.
 
-// check whether highlights are showing
+// Options
+var annotationOptions = {
+    highlightsColor: '#eee'
+};
 
-// toggle highlights on
+// Get the elements we need
+var openSidebarToggle = document.querySelector('#annotator-toggle-sidebar');
+var showAnnotationsToggle = document.querySelector('#annotator-show-annotations');
 
-// open sidebar
+// Turn highlighting on
+function showAnnotationHighlights() {
+    'use strict';
+    var css = '.annotator-hl{background-color:' + annotationOptions.highlightsColor + '}';
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = 'annotation-styles';
 
-// listen for toggle on annotation-toggle
+    if (style.styleSheet) {
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+    document.head.appendChild(style);
+}
 
-// listen for toggle
-function listenForToggle(target) {
+// Turn highlighting off
+function hideAnnotationHighlights() {
+    'use strict';
+    if (document.querySelector('#annotation-styles')) {
+        document.querySelector('#annotation-styles').remove();
+    }
+}
+
+// Open sidebar
+function annotatorOpenSidebar() {
+    'use strict';
+    openSidebarToggle.checked = true;
+}
+
+// Close sidebar
+function annotatorCloseSidebar() {
+    'use strict';
+    openSidebarToggle.checked = false;
+}
+
+// Listen for checkbox toggle. This function takes three arguments:
+// target: the checkbox to listen on,
+// on: the function if it's checked,
+// off: the function if it's not checked.
+function annotatorListenForToggle(target, on, off) {
     'use strict';
     if (target) {
         target.addEventListener('change', function () {
             if (this.checked) {
-                console.log(target + ' is checked');
+                on();
             } else {
-                console.log(target + ' is not checked');
+                off();
             }
         });
     }
 }
 
-// get the checkboxes
-var showSidebarToggle = document.querySelector('#annotator-toggle-sidebar');
-var showAnnotationsToggle = document.querySelector('#annotator-show-annotations');
+// Listen for clicks
+function annotatorListenForClick(target, action) {
+    'use strict';
+    if (target) {
+        console.log('listening?');
+        target.addEventListener('click', function () {
+            console.log('heard something');
+            action();
+        });
+    }
+}
 
-// process
-listenForToggle(showSidebarToggle);
-listenForToggle(showAnnotationsToggle);
+// Start
+window.onload = function () {
+    'use strict';
+
+    // Get the annotator button
+    // Todo: On Chrome this is in a shadowRoot. Can't get at it this way.
+    var annotatorTool = document.querySelector('.js-annotate-btn');
+
+    annotatorListenForToggle(openSidebarToggle, annotatorOpenSidebar, annotatorCloseSidebar);
+    annotatorListenForToggle(showAnnotationsToggle, showAnnotationHighlights, hideAnnotationHighlights);
+    annotatorListenForClick(annotatorTool, annotatorOpenSidebar);
+};
