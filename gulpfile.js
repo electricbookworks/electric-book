8566960ec2cc85403e64f36eac97806867ff7c54
Arthur Attwell <arthur@arthurattwell.com>
2019-12-09 17:52:41 +0200

    Check for existence of file before calling it
    This is mainly so that we can use this gulpfile as-is in the
    electric-book-media repo for external media, where there
    is no meta.yml file.

--- gulpfile.js
+++ gulpfile.js
@@ -23,40 +23,44 @@ var gulp = require('gulp'),
     cheerio = require('gulp-cheerio');
 
 // Get arrays of possible book and language paths
-var metadata = yaml.load(fs.readFileSync('_data/meta.yml', 'utf8'));
-var works = metadata.works;
-function loadMetadata() {
-    'use strict';
-    var paths = [];
-    var filePaths = [];
-    var books = [];
-    var languages = [];
-    var i;
-    var j;
-    for (i = 0; i < works.length; i += 1) {
-        books.push(works[i].directory);
-        paths.push('_site/' + works[i].directory + '/text/');
-        filePaths.push('_site/' + works[i].directory + '/text/*.html');
-        if (works[i].translations) {
-            for (j = 0; j < works[i].translations.length; j += 1) {
-                languages.push(works[i].translations[j].directory);
-                paths.push('_site/' + works[i].directory + '/' + works[i].translations[j].directory + '/text/');
-                filePaths.push('_site/' + works[i].directory + '/' + works[i].translations[j].directory + '/text/*.html');
+if (fileExists.sync('_data/meta.yml')) {
+    var metadata = yaml.load(fs.readFileSync('_data/meta.yml', 'utf8'));
+    var works = metadata.works;
+    function loadMetadata() {
+        'use strict';
+        var paths = [];
+        var filePaths = [];
+        var books = [];
+        var languages = [];
+        var i;
+        var j;
+        for (i = 0; i < works.length; i += 1) {
+            books.push(works[i].directory);
+            paths.push('_site/' + works[i].directory + '/text/');
+            filePaths.push('_site/' + works[i].directory + '/text/*.html');
+            if (works[i].translations) {
+                for (j = 0; j < works[i].translations.length; j += 1) {
+                    languages.push(works[i].translations[j].directory);
+                    paths.push('_site/' + works[i].directory + '/' + works[i].translations[j].directory + '/text/');
+                    filePaths.push('_site/' + works[i].directory + '/' + works[i].translations[j].directory + '/text/*.html');
+                }
             }
         }
+        return {
+            books: books,
+            languages: languages,
+            paths: paths,
+            filePaths: filePaths
+        };
     }
-    return {
-        books: books,
-        languages: languages,
-        paths: paths,
-        filePaths: filePaths
-    };
+    loadMetadata();
+}
+
 // Load image settings if they exist
 var imageSettings = [];
 if (fs.existsSync('_data/images.yml')) {
     imageSettings = yaml.load(fs.readFileSync('_data/images.yml', 'utf8'));
 }
-loadMetadata();
 
 // Get the book we're processing
 var book = 'book';
