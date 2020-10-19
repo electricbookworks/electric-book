46b8bff9bfd17be7d13e266e367d4717306b7540
Alexander Maughan <alex@maughan.me>
2020-10-19 12:48:56 +0200

    Remove logic redundancy for settings.js property

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -2,7 +2,7 @@
 
 // Fetch specific values from settings.yml and
 // convert them into a Javascript object called settings.
-// Note that some YAML keys use hyphens, which are invalid JS. 
+// Note that some YAML keys use hyphens, which are invalid JS.
 // So to use them as variables, use square brackets and quotes,
 // e.g. search['search-placeholder'].
 // NB: The generated settings load in client-side Javascript, so
@@ -31,8 +31,6 @@ settings.site.output = '{{ site.output }}';
 {% endif %}
 
 // Override default settings from settings.yml
-{% if site.data.settings.web.images.lazyload %}
+{% if site.data.settings.web.images.lazyload != nil %}
 settings.web.images.lazyload = {{ site.data.settings.web.images.lazyload }};
-{% else %}
-settings.web.images.lazyload = false;
 {% endif %}
