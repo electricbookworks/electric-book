224534f34af4454520eb2140d2701acbdf102f0c
Arthur Attwell <arthur@arthurattwell.com>
2020-12-31 14:27:05 +0200

    Lint and add guidance to copy-to-clipboard script

--- assets/js/copy-to-clipboard.js
+++ assets/js/copy-to-clipboard.js
@@ -1,5 +1,10 @@
 /*jslint browser */
-/*globals locales, pageLanguage */
+/*globals window, locales, pageLanguage */
+
+// Note: the navigator API for this script requires
+// that the page is served over https (or is localhost).
+// So this will not work on http connections.
+// Ensure your webserver is sending all traffic to https.
 
 // Set default button text.
 var ebCopyToClipboardButtonText = locales[pageLanguage].copy.copy;
@@ -15,29 +20,29 @@ function ebCopyButtonFeedback(button, text) {
         button.classList.add('copy-to-clipboard-success');
     }
 
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         button.innerHTML = ebCopyToClipboardButtonText;
         button.classList.remove('copy-to-clipboard-success');
-    }, 2000 );
+    }, 2000);
 }
 
 // Copy an element's text to the clipboard.
 function ebCopyToClipboard(element, button) {
     'use strict';
     var text = element.textContent;
-    navigator.clipboard.writeText(text).then(function() {
+    navigator.clipboard.writeText(text).then(function () {
         // success
-        ebCopyButtonFeedback(button, ebCopyToClipboardSuccessText)
-    }, function() {
+        ebCopyButtonFeedback(button, ebCopyToClipboardSuccessText);
+    }, function () {
         // failure
-        ebCopyButtonFeedback(button, ebCopyToClipboardFailText)
+        ebCopyButtonFeedback(button, ebCopyToClipboardFailText);
     });
 }
 
 // Add a copy button, ready for clicking.
 function ebAddCopyButton(element, buttonText) {
     'use strict';
-    
+
     var button = document.createElement('button');
     button.classList.add('copy-to-clipboard');
     button.setAttribute('type', 'button');
