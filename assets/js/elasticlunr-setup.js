a9cf99dc7f00a44a4f49fc294e219eb06780f80a
Arthur Attwell <arthur@arthurattwell.com>
2023-07-31 13:07:01 +0200

    Initialise variable to avoid errors

--- assets/js/elasticlunr-setup.js
+++ assets/js/elasticlunr-setup.js
@@ -5,8 +5,9 @@
 // to run in Node when rendering the search index.
 // We initialise `index` here, and then add docs
 // to elasticlunr's index elsewhere with index.addDoc().
+let index
 if (typeof window !== 'undefined') {
-  const index = elasticlunr(function () { // eslint-disable-line
+  index = elasticlunr(function () { // eslint-disable-line
     this.addField('title')
     this.addField('content')
   })
