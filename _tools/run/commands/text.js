6ffb3bc81411a6a4d422340a391e0fcf57b1b4e3
Arthur Attwell <arthur@arthurattwell.com>
2024-10-08 19:46:44 +0200

    Update function names in code comments

--- _tools/run/commands/text.js
+++ _tools/run/commands/text.js
@@ -17,11 +17,11 @@ exports.handler = function (argv) {
 
   if (filetype) {
     if (filetype === 'docx') {
-      // If the source is a Word file, run importText
+      // If the source is a Word file, run convertToMarkdown()
       // which will also split if the --split arg is passed.
       convertToMarkdown(argv)
     } else if (filetype === 'md' && explicitOption('split')) {
-      // If the source is a markdown file, don't run importText.
+      // If the source is a markdown file, don't run convertToMarkdown()
       // but do run splitMarkdownFile if --split is given.
       splitMarkdownFile(argv)
     } else {
